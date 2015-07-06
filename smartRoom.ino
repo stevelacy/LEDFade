bd975752df4ea1f692d9d73889a5d0d41178b713
Steve Lacy <me@slacy.me>
2015-07-06 16:22:47 -0700

    init

---
+++ smartRoom.ino
@@ -0,0 +1,34 @@
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+#define PIN     3
+#define LEDNUM  7
+#define DELAY   50
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDNUM, PIN, NEO_GRB + NEO_KHZ800);
+
+const int buttonPin = 2;
+int full = 0;
+
+void setup() {
+  strip.begin();
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  //buttonState = digitalRead(buttonPin);
+
+  for (int i = 0; i <= 255; i++) {
+    if (full == 1) {
+      return;
+    }
+    for (int l=0; l < strip.numPixels(); l++) {
+      strip.setPixelColor(l, strip.Color(i,i,0));
+    }
+    strip.show();
+    delay(10);
+    if (i == 255) {
+      full = 1;
+    }
+  }
+}
